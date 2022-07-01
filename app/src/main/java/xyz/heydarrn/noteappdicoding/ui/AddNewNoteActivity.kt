9ddeb848b5b185072be9e5e19622d3a0c120982b
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2022-07-01 09:35:14 +0700

    feature: menambahkan activity untuk tambah note dan update note

---
+++ app/src/main/java/xyz/heydarrn/noteappdicoding/ui/AddNewNoteActivity.kt
@@ -0,0 +1,12 @@
+package xyz.heydarrn.noteappdicoding.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import xyz.heydarrn.noteappdicoding.R
+
+class AddNewNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_note)
+    }
+}
---
+++ app/src/main/java/xyz/heydarrn/noteappdicoding/ui/UpdateNoteActivity.kt
@@ -0,0 +1,12 @@
+package xyz.heydarrn.noteappdicoding.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import xyz.heydarrn.noteappdicoding.R
+
+class UpdateNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_note)
+    }
+}
